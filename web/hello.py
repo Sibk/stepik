a6972c6e7e5a3d76132fd019fa5add562a170807
Sibk <siberuys2012@gmail.com>
2018-06-18 21:04:44 +0000

    nginx5

--- web/hello.py
+++ web/hello.py
@@ -4,6 +4,6 @@ def hello_world(env, response):
 	res = env['QUERY_STRING'].split('&')
 	res = [bytes(x + '\n', 'ascii') for x in res]
 	head = [('Content-Type', 'text/plain')]
-	status = bytes('200 OK', 'ascii')
+	status = '200 OK'
 	response(status, head)
 	return res
