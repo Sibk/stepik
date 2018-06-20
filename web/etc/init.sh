9d17a11a4841c2823e785aa201126bddae566400
Sibk <siberuys2012@gmail.com>
2018-06-20 09:46:22 +0000

    django

--- web/etc/init.sh
+++ web/etc/init.sh
@@ -2,3 +2,4 @@ sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/gun-django.conf ask.wsgi:application
