14a7944974ee97017ad40019bcd52149521ac7a5
Sibk <siberuys2012@gmail.com>
2018-06-23 09:20:43 +0000

    dajngo_m

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,5 +1,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Question(models.Model):
+#	objects = QuestionManager()
+	title = models.CharField(max_length = 100)
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	rating = models.IntegerField()
+	author = models.ForeignKey(User, default = 'x', on_delete = models.CASCADE)
+	likes = models.ManyToManyField(User, related_name = 'user_likes')
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField(auto_now_add=True)
+	question = models.ForeignKey(Question, on_delete = models.CASCADE)
+	author = models.ForeignKey(User, default = 'x', on_delete = models.CASCADE)
+
+class QuestionManager:
+	def new(self):
+		que = Question.objects.filter(Max(added_at))
+		return que
+	def popular(self):
+		popular = Question.objects.all().order_by('-rating')
+		return popular
